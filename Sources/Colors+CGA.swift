c12a9c42e5758c200e13c6759e08bfbfc12be470
Dylan Wreggelsworth <dylan@bvr.io>
2017-04-12 15:21:55 +0200

    Added X11, CGA, W3C, and UIColor named colors.

---
+++ Sources/Colors+CGA.swift
@@ -0,0 +1,40 @@
+//
+//  Colors+CGA.swift
+//  Colors
+//
+//  Created by Dylan Wreggelsworth on 4/12/17.
+//  Copyright © 2017 Colors. All rights reserved.
+//
+
+public enum CGA {
+    
+    public static let black        = Color(rgb: 0x000000)
+    
+    public static let blue         = Color(rgb: 0x0000aa)
+    
+    public static let green        = Color(rgb: 0x00aa00)
+    
+    public static let cyan         = Color(rgb: 0x00aaaa)
+    
+    public static let magenta      = Color(rgb: 0xaa00aa)
+    
+    public static let brown        = Color(rgb: 0xaa5500)
+    
+    public static let lightGray    = Color(rgb: 0xaaaaaa)
+    
+    public static let gray         = Color(rgb: 0x555555)
+    
+    public static let lightBlue    = Color(rgb: 0x5555ff)
+    
+    public static let lightGreen   = Color(rgb: 0x55ff55)
+    
+    public static let lightCyan    = Color(rgb: 0x55ffff)
+    
+    public static let lightRed     = Color(rgb: 0xff5555)
+    
+    public static let lightMagenta = Color(rgb: 0xff55ff)
+    
+    public static let lightYellow  = Color(rgb: 0xffff55)
+    
+    public static let white        = Color(rgb: 0xffffff)
+}
---
+++ Sources/Colors+X11.swift
@@ -0,0 +1,290 @@
+//
+//  Color+X11.swift
+//  Colors
+//
+//  Created by Dylan Wreggelsworth on 4/12/17.
+//  Copyright © 2017 Colors. All rights reserved.
+//
+
+public enum X11 {
+    
+    public static let lightPink            = Color(rgb: 0xffb6c1)
+    
+    public static let pink                 = Color(rgb: 0xffc0cb)
+    
+    public static let crimson              = Color(rgb: 0xdc143c)
+    
+    public static let lavenderBlush        = Color(rgb: 0xfff0f5)
+    
+    public static let paleVioletRed        = Color(rgb: 0xdb7093)
+    
+    public static let hotPink              = Color(rgb: 0xff69b4)
+    
+    public static let deepPink             = Color(rgb: 0xff1493)
+    
+    public static let mediumVioletRed      = Color(rgb: 0xc71585)
+    
+    public static let orchid               = Color(rgb: 0xda70d6)
+    
+    public static let thistle              = Color(rgb: 0xd8bfd8)
+    
+    public static let plum                 = Color(rgb: 0xdda0dd)
+    
+    public static let violet               = Color(rgb: 0xee82ee)
+    
+    public static let magenta              = Color(rgb: 0xff00ff)
+    
+    public static let fuchsia              = Color(rgb: 0xff00ff)
+    
+    public static let darkMagenta          = Color(rgb: 0x8b008b)
+    
+    public static let purple               = Color(rgb: 0x800080)
+    
+    public static let mediumOrchid         = Color(rgb: 0xba55d3)
+    
+    public static let darkViolet           = Color(rgb: 0x9400d3)
+    
+    public static let darkOrchid           = Color(rgb: 0x9932cc)
+    
+    public static let indigo               = Color(rgb: 0x4b0082)
+    
+    public static let blueViolet           = Color(rgb: 0x8a2be2)
+    
+    public static let mediumPurple         = Color(rgb: 0x9370db)
+    
+    public static let mediumSlateBlue      = Color(rgb: 0x7b68ee)
+    
+    public static let slateBlue            = Color(rgb: 0x6a5acd)
+    
+    public static let darkSlateBlue        = Color(rgb: 0x483d8b)
+    
+    public static let lavender             = Color(rgb: 0xe6e6fa)
+    
+    public static let ghostWhite           = Color(rgb: 0xf8f8ff)
+    
+    public static let blue                 = Color(rgb: 0x0000ff)
+    
+    public static let mediumBlue           = Color(rgb: 0x0000cd)
+    
+    public static let midnightBlue         = Color(rgb: 0x191970)
+    
+    public static let darkBlue             = Color(rgb: 0x00008b)
+    
+    public static let navy                 = Color(rgb: 0x000080)
+    
+    public static let royalBlue            = Color(rgb: 0x4169e1)
+    
+    public static let cornflowerBlue       = Color(rgb: 0x6495ed)
+    
+    public static let lightSteelBlue       = Color(rgb: 0xb0c4de)
+    
+    public static let lightSlateGray       = Color(rgb: 0x778899)
+    
+    public static let slateGray            = Color(rgb: 0x708090)
+    
+    public static let dodgerBlue           = Color(rgb: 0x1e90ff)
+    
+    public static let aliceBlue            = Color(rgb: 0xf0f8ff)
+    
+    public static let steelBlue            = Color(rgb: 0x4682b4)
+    
+    public static let lightSkyBlue         = Color(rgb: 0x87cefa)
+    
+    public static let skyBlue              = Color(rgb: 0x87ceeb)
+    
+    public static let deepSkyBlue          = Color(rgb: 0x00bfff)
+    
+    public static let lightBlue            = Color(rgb: 0xadd8e6)
+    
+    public static let powderBlue           = Color(rgb: 0xb0e0e6)
+    
+    public static let cadetBlue            = Color(rgb: 0x5f9ea0)
+    
+    public static let azure                = Color(rgb: 0xf0ffff)
+    
+    public static let lightCyan            = Color(rgb: 0xe0ffff)
+    
+    public static let paleTurquoise        = Color(rgb: 0xafeeee)
+    
+    public static let cyan                 = Color(rgb: 0x00ffff)
+    
+    public static let aqua                 = Color(rgb: 0x00ffff)
+    
+    public static let darkTurquoise        = Color(rgb: 0x00ced1)
+    
+    public static let darkSlateGray        = Color(rgb: 0x2f4f4f)
+    
+    public static let darkCyan             = Color(rgb: 0x008b8b)
+    
+    public static let teal                 = Color(rgb: 0x008080)
+    
+    public static let mediumTurquoise      = Color(rgb: 0x48d1cc)
+    
+    public static let lightSeaGreen        = Color(rgb: 0x20b2aa)
+    
+    public static let turquoise            = Color(rgb: 0x40e0d0)
+    
+    public static let aquamarine           = Color(rgb: 0x7fffd4)
+    
+    public static let mediumAquamarine     = Color(rgb: 0x66cdaa)
+    
+    public static let mediumSpringGreen    = Color(rgb: 0x00fa9a)
+    
+    public static let mintCream            = Color(rgb: 0xf5fffa)
+    
+    public static let springGreen          = Color(rgb: 0x00ff7f)
+    
+    public static let mediumSeaGreen       = Color(rgb: 0x3cb371)
+    
+    public static let seaGreen             = Color(rgb: 0x2e8b57)
+    
+    public static let honeydew             = Color(rgb: 0xf0fff0)
+    
+    public static let lightGreen           = Color(rgb: 0x90ee90)
+    
+    public static let paleGreen            = Color(rgb: 0x98fb98)
+    
+    public static let darkSeaGreen         = Color(rgb: 0x8fbc8f)
+    
+    public static let limeGreen            = Color(rgb: 0x32cd32)
+    
+    public static let lime                 = Color(rgb: 0x00ff00)
+    
+    public static let forestGreen          = Color(rgb: 0x228b22)
+    
+    public static let green                = Color(rgb: 0x008000)
+    
+    public static let darkGreen            = Color(rgb: 0x006400)
+    
+    public static let chartreuse           = Color(rgb: 0x7fff00)
+    
+    public static let lawnGreen            = Color(rgb: 0x7cfc00)
+    
+    public static let greenYellow          = Color(rgb: 0xadff2f)
+    
+    public static let darkOliveGreen       = Color(rgb: 0x556b2f)
+    
+    public static let yellowGreen          = Color(rgb: 0x9acd32)
+    
+    public static let oliveDrab            = Color(rgb: 0x6b8e23)
+    
+    public static let beige                = Color(rgb: 0xf5f5dc)
+    
+    public static let lightGoldenrodYellow = Color(rgb: 0xfafad2)
+    
+    public static let ivory                = Color(rgb: 0xfffff0)
+    
+    public static let lightYellow          = Color(rgb: 0xffffe0)
+    
+    public static let yellow               = Color(rgb: 0xffff00)
+    
+    public static let olive                = Color(rgb: 0x808000)
+    
+    public static let darkKhaki            = Color(rgb: 0xbdb76b)
+    
+    public static let lemonChiffon         = Color(rgb: 0xfffacd)
+    
+    public static let paleGoldenrod        = Color(rgb: 0xeee8aa)
+    
+    public static let khaki                = Color(rgb: 0xf0e68c)
+    
+    public static let gold                 = Color(rgb: 0xffd700)
+    
+    public static let cornsilk             = Color(rgb: 0xfff8dc)
+    
+    public static let goldenrod            = Color(rgb: 0xdaa520)
+    
+    public static let darkGoldenrod        = Color(rgb: 0xb8860b)
+    
+    public static let floralWhite          = Color(rgb: 0xfffaf0)
+    
+    public static let oldLace              = Color(rgb: 0xfdf5e6)
+    
+    public static let wheat                = Color(rgb: 0xf5deb3)
+    
+    public static let moccasin             = Color(rgb: 0xffe4b5)
+    
+    public static let orange               = Color(rgb: 0xffa500)
+    
+    public static let papayaWhip           = Color(rgb: 0xffefd5)
+    
+    public static let blanchedAlmond       = Color(rgb: 0xffebcd)
+    
+    public static let navajoWhite          = Color(rgb: 0xffdead)
+    
+    public static let antiqueWhite         = Color(rgb: 0xfaebd7)
+    
+    public static let tan                  = Color(rgb: 0xd2b48c)
+    
+    public static let burlyWood            = Color(rgb: 0xdeb887)
+    
+    public static let bisque               = Color(rgb: 0xffe4c4)
+    
+    public static let darkOrange           = Color(rgb: 0xff8c00)
+    
+    public static let linen                = Color(rgb: 0xfaf0e6)
+    
+    public static let peru                 = Color(rgb: 0xcd853f)
+    
+    public static let peachPuff            = Color(rgb: 0xffdab9)
+    
+    public static let sandyBrown           = Color(rgb: 0xf4a460)
+    
+    public static let chocolate            = Color(rgb: 0xd2691e)
+    
+    public static let saddleBrown          = Color(rgb: 0x8b4513)
+    
+    public static let seashell             = Color(rgb: 0xfff5ee)
+    
+    public static let sienna               = Color(rgb: 0xa0522d)
+    
+    public static let lightSalmon          = Color(rgb: 0xffa07a)
+    
+    public static let coral                = Color(rgb: 0xff7f50)
+    
+    public static let orangeRed            = Color(rgb: 0xff4500)
+    
+    public static let darkSalmon           = Color(rgb: 0xe9967a)
+    
+    public static let tomato               = Color(rgb: 0xff6347)
+    
+    public static let mistyRose            = Color(rgb: 0xffe4e1)
+    
+    public static let salmon               = Color(rgb: 0xfa8072)
+    
+    public static let snow                 = Color(rgb: 0xfffafa)
+    
+    public static let lightCoral           = Color(rgb: 0xf08080)
+    
+    public static let rosyBrown            = Color(rgb: 0xbc8f8f)
+    
+    public static let indianRed            = Color(rgb: 0xcd5c5c)
+    
+    public static let red                  = Color(rgb: 0xff0000)
+    
+    public static let brown                = Color(rgb: 0xa52a2a)
+    
+    public static let fireBrick            = Color(rgb: 0xb22222)
+    
+    public static let darkRed              = Color(rgb: 0x8b0000)
+    
+    public static let maroon               = Color(rgb: 0x800000)
+    
+    public static let white                = Color(rgb: 0xffffff)
+    
+    public static let whiteSmoke           = Color(rgb: 0xf5f5f5)
+    
+    public static let gainsboro            = Color(rgb: 0xdcdcdc)
+    
+    public static let lightGrey            = Color(rgb: 0xd3d3d3)
+    
+    public static let silver               = Color(rgb: 0xc0c0c0)
+    
+    public static let darkGray             = Color(rgb: 0xa9a9a9)
+    
+    public static let gray                 = Color(rgb: 0x808080)
+    
+    public static let dimGray              = Color(rgb: 0x696969)
+    
+    public static let black                = Color(rgb: 0x000000)
+}
