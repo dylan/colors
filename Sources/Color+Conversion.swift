88641f3f3f38fd9d5f6d4eb3a291d3973e22384d
phlebotinum <boris@1up-productions.com>
2017-08-16 16:20:15 +0200

    Removes dead code.

--- Sources/Color+Conversion.swift
+++ Sources/Color+Conversion.swift
@@ -282,7 +282,6 @@ extension Color {
         let lmin = (2 - s) * vmin
         var sl = s * vmin
         sl /= lmin <= 1 ? lmin : 2 - lmin
-//        sl = sl < 0 ? 0 : sl
         if sl.isNaN { sl = 0.0 }
         l /= 2
 
